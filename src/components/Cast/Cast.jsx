819ddbe9fa370b2b4a914a23df102b35c0dbd8fa
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2024-04-17 21:43:35 +0300

    new styles fro cast + adaptive

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { getMovieCastActors } from 'helpers/api';
-import { List, Card, Img, Text, TextCont } from './Cast.styled';
+import { Card, Img, Text, TextCont } from './Cast.styled';
+// import { List, Card, Img, Text, TextCont } from './Cast.styled';
 import defaultImage from 'assets/popcorn.jpg';
+import { List, Item, CoverImage, Title } from 'pages/CommonStyles.styled';
 
 const baseUrl = 'https://image.tmdb.org/t/p/w300';
 
@@ -21,24 +23,42 @@ const Cast = () => {
   return cast.length !== 0 ? (
     <List>
       {cast.map(({ id, profile_path, character, name }) => (
-        <Link key={id} to={`/actors/${id}`}>
-          <Card>
-            <Img
+        // <Link key={id} to={`/actors/${id}`}>
+        //   <Card>
+        //     <Img
+        //       src={profile_path ? `${baseUrl}${profile_path}` : defaultImage}
+        //       alt={name}
+        //       width="150"
+        //       height="250"
+        //     />
+        //     <TextCont>
+        //       <Text>
+        //         Character: <span>{character}</span>
+        //       </Text>
+        //       <Text>
+        //         Actor: <span>{name}</span>
+        //       </Text>
+        //     </TextCont>
+        //   </Card>
+        // </Link>
+        <Item key={id}>
+          <Link to={`/actors/${id}`}>
+            <CoverImage
               src={profile_path ? `${baseUrl}${profile_path}` : defaultImage}
               alt={name}
               width="150"
               height="250"
             />
             <TextCont>
-              <Text>
+              <Title>
                 Character: <span>{character}</span>
-              </Text>
-              <Text>
+              </Title>
+              <Title>
                 Actor: <span>{name}</span>
-              </Text>
+              </Title>
             </TextCont>
-          </Card>
-        </Link>
+          </Link>
+        </Item>
       ))}
     </List>
   ) : (

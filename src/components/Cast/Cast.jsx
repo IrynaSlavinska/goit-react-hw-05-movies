e59994e3b3dce8b72cb47eceec000d23acb3f7fa
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2024-04-17 21:46:53 +0300

    removed commented code

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { getMovieCastActors } from 'helpers/api';
-import { Card, Img, Text, TextCont } from './Cast.styled';
-// import { List, Card, Img, Text, TextCont } from './Cast.styled';
 import defaultImage from 'assets/popcorn.jpg';
 import { List, Item, CoverImage, Title } from 'pages/CommonStyles.styled';
 
@@ -23,24 +21,6 @@ const Cast = () => {
   return cast.length !== 0 ? (
     <List>
       {cast.map(({ id, profile_path, character, name }) => (
-        // <Link key={id} to={`/actors/${id}`}>
-        //   <Card>
-        //     <Img
-        //       src={profile_path ? `${baseUrl}${profile_path}` : defaultImage}
-        //       alt={name}
-        //       width="150"
-        //       height="250"
-        //     />
-        //     <TextCont>
-        //       <Text>
-        //         Character: <span>{character}</span>
-        //       </Text>
-        //       <Text>
-        //         Actor: <span>{name}</span>
-        //       </Text>
-        //     </TextCont>
-        //   </Card>
-        // </Link>
         <Item key={id}>
           <Link to={`/actors/${id}`}>
             <CoverImage
@@ -49,14 +29,12 @@ const Cast = () => {
               width="150"
               height="250"
             />
-            <TextCont>
-              <Title>
-                Character: <span>{character}</span>
-              </Title>
-              <Title>
-                Actor: <span>{name}</span>
-              </Title>
-            </TextCont>
+            <Title>
+              Character: <span>{character}</span>
+            </Title>
+            <Title>
+              Actor: <span>{name}</span>
+            </Title>
           </Link>
         </Item>
       ))}

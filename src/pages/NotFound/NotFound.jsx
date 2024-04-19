57f52d0a891a3b21e55f1e93cb47904017997a99
Iryna Slavinska <lanfirtiger.is@gmail.com>
2024-04-20 01:27:12 +0300

    remive icon crab

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,13 +1,8 @@
 import { Title, Text, StyledNav, Container } from './NotFound.styled';
-import { GiSadCrab } from 'react-icons/gi';
-import { IconContext } from 'react-icons';
 
 const NotFound = () => {
   return (
     <Container>
-      <IconContext.Provider value={{ color: 'rgb(113, 113, 218)', size: 250 }}>
-        <GiSadCrab />
-      </IconContext.Provider>
       <Title>404</Title>
       <Text>You have gone to a non existing page</Text>
       <Text>Return to the</Text>

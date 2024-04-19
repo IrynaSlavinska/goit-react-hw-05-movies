0146a797e9be96e75dc4bda3cd5b399ff3d958c4
Iryna Slavinska <lanfirtiger.is@gmail.com>
2024-04-20 01:31:04 +0300

    adaptive photo gallery

--- src/components/Gallery/Gallery.styled.jsx
+++ src/components/Gallery/Gallery.styled.jsx
@@ -4,7 +4,25 @@ export const List = styled.ul`
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
-  justify-content: center;
+
+  @media screen and (min-width: 768px) {
+    gap: 40px 32px;
+  }
 `;
 
-export const Item = styled.li``;
+export const Item = styled.li`
+  background-color: rgb(50, 50, 87);
+  display: flex;
+  justify-content: center;
+  border-radius: 8px;
+  width: 100%;
+  padding: 10px;
+
+  @media screen and (min-width: 768px) {
+    width: calc((100% - 32px) / 2);
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: calc((100% - 32px * 4) / 5);
+  }
+`;

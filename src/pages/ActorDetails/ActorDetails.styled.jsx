8c16d0640305ca4b34b4e1737bb1998ec9e6bbbb
Iryna Slavinska <lanfirtiger.is@gmail.com>
2024-04-20 01:29:21 +0300

    new styles + change colors

--- src/pages/ActorDetails/ActorDetails.styled.jsx
+++ src/pages/ActorDetails/ActorDetails.styled.jsx
@@ -2,39 +2,53 @@ import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
 export const StyledNav = styled(NavLink)`
-  color: #ffffff;
+  color: var(--white);
   font-size: 20px;
   &:hover {
-    color: rgb(113, 113, 218);
+    color: var(--yellow);
+  }
+  &.active {
+    color: var(--yellow);
   }
 `;
+
 export const ActorCard = styled.div`
-  padding: 50px;
-  display: flex;
-  align-items: center;
-  gap: 40px;
+  border: 1px solid var(--yellow);
+  border-radius: 4px;
+  box-shadow: 0 0 5px var(--yellow), 0 0 5px var(--yellow) inset;
+  margin-bottom: 20px;
+  padding: 20px;
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    gap: 40px;
+  }
 `;
+
 export const ActorData = styled.div``;
 
 export const Name = styled.h2`
-  color: #ffffff;
+  color: var(--yellow);
   font-size: 28px;
-  margin-bottom: 32px;
+  margin-top: 20px;
+  margin-bottom: 20px;
 `;
 
 export const Info = styled.h3`
-  color: #ffffff;
+  color: var(--yellow);
   margin-bottom: 4px;
 `;
 
 export const Text = styled.p`
-  color: #ffffff;
+  color: var(--white);
   margin-bottom: 16px;
   line-height: 1.5;
 `;
 
 export const List = styled.ul`
   display: flex;
+  justify-content: center;
   gap: 20px;
-  margin-bottom: 12px;
+  margin-bottom: 20px;
 `;

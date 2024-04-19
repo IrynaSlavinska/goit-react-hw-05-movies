6b9ab9f4a78e843132c6c3e87aa7a8ae985a08b7
Iryna Slavinska <lanfirtiger.is@gmail.com>
2024-04-20 00:18:59 +0300

    show btn styles

--- src/components/Reviews/Revies.styled.jsx
+++ src/components/Reviews/Revies.styled.jsx
@@ -16,9 +16,21 @@ export const Item = styled.li`
 export const Author = styled.h3`
   color: var(--white);
   font-size: 20px;
-  margin-bottom: 8px;
+  margin-bottom: 12px;
 `;
 
 export const Text = styled.p`
+  font-size: 18px;
+  line-height: 1.5;
   color: var(--white);
+`;
+
+export const ToggleButton = styled.button`
+  padding: 4px 12px;
+  margin-left: 8px;
+  font-size: 18px;
+  color: var(--yellow);
+  border: 1px solid var(--yellow);
+  border-radius: 4px;
+  box-shadow: 0 0 5px var(--yellow), 0 0 5px var(--yellow) inset;
 `;

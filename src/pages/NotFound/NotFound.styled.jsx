7c540bc7f0ef45dd3773a9da37647a8c9cf0c7fd
Iryna Slavinska <lanfirtiger.is@gmail.com>
2024-04-20 01:26:51 +0300

    bcg error page

--- src/pages/NotFound/NotFound.styled.jsx
+++ src/pages/NotFound/NotFound.styled.jsx
@@ -1,24 +1,44 @@
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
+import sadCat from 'assets/sad-cat.webp';
+import defaultImage from 'assets/popcorn.jpg';
+import errorTablet from 'assets/error.jpg';
+
 export const Container = styled.div`
-  padding: 50px;
+  padding: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   gap: 12px;
+  background-image: url(${defaultImage});
+  max-width: 1380px;
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  margin: 0 auto;
+  height: calc(100vh - 140px);
+
+  @media screen and (min-width: 768px) {
+    background-image: url(${errorTablet});
+  }
+
+  @media screen and (min-width: 1440px) {
+    background-image: url(${sadCat});
+  }
 `;
 
 export const Title = styled.h1`
-  color: #ffffff;
+  color: var(--white);
 `;
 
 export const Text = styled.p`
-  color: #ffffff;
+  color: var(--white);
 `;
 
 export const StyledNav = styled(NavLink)`
-  color: rgb(113, 113, 218);
+  color: var(--yellow);
   font-size: 40px;
 `;
